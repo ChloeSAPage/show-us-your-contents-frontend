3c5133bf4652b720d7cdcc294e36b2ca39927a72
Jo Pickering <joanna.r.pickering@gmail.com>
2024-07-04 16:57:11 +0100

    fixed nav link double highlighting issue

--- src/components/ui/site-header/SiteHeader.jsx
+++ src/components/ui/site-header/SiteHeader.jsx
@@ -4,53 +4,53 @@ import { logout } from "../../../slices/authSlice";
 import styles from "./site-header.module.css";
 
 function SiteHeader() {
-	const dispatch = useDispatch();
-	const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
-	const handleLogout = () => {
-		dispatch(logout());
-	};
+  const handleLogout = () => {
+    dispatch(logout());
+  };
 
-	const navLinks = [];
-	if (isLoggedIn) {
-		navLinks.push(
-			{ label: "My things", url: "/my-things/" },
-			{ label: "Add bag", url: "/my-things/add/" },
-			{ label: "Add treasure", url: "/treasures/add/" }
-		);
-	} else {
-		navLinks.push(
-			{ label: "Login", url: "/login/" },
-			{ label: "Register", url: "/register/" }
-		);
-	}
+  const navLinks = [];
+  if (isLoggedIn) {
+    navLinks.push(
+      { label: "My things", url: "/my-things/" },
+      { label: "Add bag", url: "/bags/add/" },
+      { label: "Add treasure", url: "/treasures/add/" }
+    );
+  } else {
+    navLinks.push(
+      { label: "Login", url: "/login/" },
+      { label: "Register", url: "/register/" }
+    );
+  }
 
-	return (
-		<div className={styles.wrapper}>
-			<img src="/suyc_logo.png" alt="Logo" className={styles.logo} />
-			<h1 className={styles.title}>Show Us Your Contents</h1>
-			<div className={styles.navcontainer}>
-				<nav className={styles.links}>
-					{navLinks.map((navLink) => (
-						<NavLink
-							key={navLink.url}
-							to={navLink.url}
-							className={({ isActive }) =>
-								isActive ? styles.activeLink : styles.inactiveLink
-							}
-						>
-							{navLink.label}
-						</NavLink>
-					))}
-					{isLoggedIn && (
-						<button className={styles.logoutbutton} onClick={handleLogout}>
-							Logout
-						</button>
-					)}
-				</nav>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.wrapper}>
+      <img src="/suyc_logo.png" alt="Logo" className={styles.logo} />
+      <h1 className={styles.title}>Show Us Your Contents</h1>
+      <div className={styles.navcontainer}>
+        <nav className={styles.links}>
+          {navLinks.map((navLink) => (
+            <NavLink
+              key={navLink.url}
+              to={navLink.url}
+              className={({ isActive }) =>
+                isActive ? styles.activeLink : styles.inactiveLink
+              }
+            >
+              {navLink.label}
+            </NavLink>
+          ))}
+          {isLoggedIn && (
+            <button className={styles.logoutbutton} onClick={handleLogout}>
+              Logout
+            </button>
+          )}
+        </nav>
+      </div>
+    </div>
+  );
 }
 
 export default SiteHeader;
